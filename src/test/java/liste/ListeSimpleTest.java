a4a50049108a1a4c23d90cedc2e6a51fbdadecd7
Evan MASSOL <evan.massol@alumni.univ-avignon.fr>
2025-09-18 15:41:02 +0200

    Another potential fix

--- src/test/java/liste/ListeSimpleTest.java
+++ src/test/java/liste/ListeSimpleTest.java
@@ -249,10 +249,10 @@ class ListeSimpleTest {
         listeATester.ajout(3);
         listeATester.ajout(2);
         listeATester.ajout(1);
-        Noeud r2 = listeATester.getPrecedent(r1);
-        assertEquals("ListeSimple(Noeud(1), Noeud(2), Noeud(3), Noeud(4), Noeud(5))", listeATester.toString());
+        Noeud r2 = listeATester.tete;
+        assertEquals(listeATester.toString(), "ListeSimple(Noeud(1), Noeud(2), Noeud(3), Noeud(4), Noeud(5))");
         listeATester.echanger(r1, r2);
         System.out.println(listeATester);
-        assertEquals("ListeSimple(Noeud(1), Noeud(2), Noeud(3), Noeud(5), Noeud(4))", listeATester.toString());
+        assertEquals(listeATester.toString(), "ListeSimple(Noeud(4), Noeud(2), Noeud(3), Noeud(1), Noeud(5))");
     }
 }
